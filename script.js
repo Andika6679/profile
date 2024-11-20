024ee8ae6831d94434b6db879a25ac5a33dd7702
Andika6679 <114057440+Andika6679@users.noreply.github.com>
2024-11-20 19:45:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const playPauseButton = document.getElementById("play-pause");
+const audio = document.getElementById("audio");
+const albumImage = document.querySelector(".album-image");
+
+let isPlaying = false;
+
+playPauseButton.addEventListener("click", () => {
+    if (isPlaying) {
+        audio.pause();
+        playPauseButton.textContent = "Play";
+        albumImage.style.animationPlayState = "paused";
+    } else {
+        audio.play();
+        playPauseButton.textContent = "Pause";
+        albumImage.style.animationPlayState = "running";
+    }
+    isPlaying = !isPlaying;
+});
 
